20acfe09155e9492a3ba3d68d859d4a6cf8dd180
Konstantin Tsistjakov <ktsistjakov@gmail.com>
2017-01-24 10:09:35 +0200

    Initial Commit

---
+++ Transition/Transition.h
@@ -0,0 +1,19 @@
+//
+//  Transition.h
+//  Transition
+//
+//  Created by Konstantin Tsistjakov on 24/01/2017.
+//  Copyright © 2017 Chipp Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Transition.
+FOUNDATION_EXPORT double TransitionVersionNumber;
+
+//! Project version string for Transition.
+FOUNDATION_EXPORT const unsigned char TransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Transition/PublicHeader.h>
+
+
